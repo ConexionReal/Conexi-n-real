35dda5c52c14fba147b091cd42b020f382efd9e3
ConexionReal <carlos.perez.escabias@gmail.com>
2025-08-05 00:11:19 +0200

    Delete pago.js

--- pago.js
+++
@@ -1,9 +0,0 @@
-export default function Pago() {
-  return (
-    <main style={{ padding: 40, fontFamily: 'sans-serif' }}>
-      <h2>Activar Premium</h2>
-      <p>Precio: 5€ por 7 días</p>
-      <div id="paypal-button-container">Aquí irá el botón de PayPal</div>
-    </main>
-  )
-}
