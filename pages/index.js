f6a62b892019de9dbc141b6bdb51b808e82baac1
Carlosjpe79 <carlos.perez.escabias@gmail.com>
2025-08-25 00:23:11 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import { supabase } from '../lib/supabase'
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Conexión Real 🔮</h1>
+      <p>Bienvenido a tu app con Supabase y Next.js</p>
+    </div>
